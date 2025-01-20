b887e26d101203f597dca369f457b7b86e4f9002
ammineniashok0 <ammineniashok0@gmail.com>
2025-01-20 13:05:53 +0000

    list changes

--- routes/List.js
+++ routes/List.js
@@ -3,10 +3,10 @@
 const StatusCodeModel = require('../models/StatusCodes')
 
 const getList = async (req, res) => {
-        const list = await StatusCodeModel.find({})
-        console.log('inside the getlist', list)
-        res.statusCode = 200
-        return res.send({message:'sucessfully retrived',data:list})
+    const list = await StatusCodeModel.find({})
+    console.log('inside the getlist', list)
+    res.statusCode = 200
+    return res.send({ message: 'sucessfully retrived', data: list })
 }
 
-module.exports  = getList
+module.exports = getList
